a5694f1ef2b1f211ca792fb7129c5b4fcd08c68e
GenSteel87 <gensteel87@gmail.com>
2023-11-08 15:54:35 +0300

    Checkboxes test

--- src/test/java/AddRemoveElementsTest.java
+++ src/test/java/AddRemoveElementsTest.java
@@ -9,20 +9,23 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class AddRemoveElementsTest {
     WebDriver driver;
+
     @BeforeMethod
-    public  void setup() {
+    public void setup() {
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("start-maximized");
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
+
     @Test
-    public void addRemoveElements(){
+    public void addRemoveElements() {
         driver.get("https://the-internet.herokuapp.com/");
         driver.findElement(By.xpath("//a[text()= 'Add/Remove Elements']")).click();
         WebElement addElementButton = driver.findElement(By.xpath("//button[text()= 'Add Element']"));
@@ -32,8 +35,27 @@ public class AddRemoveElementsTest {
 
         Assert.assertEquals(driver.findElements(By.xpath("//button[text()= 'Delete']")).size(), 1);
     }
+
+    @Test
+    public void checkboxes() {
+        driver.get("https://the-internet.herokuapp.com/");
+        driver.findElement(By.xpath("//a[text()= 'Checkboxes']")).click();
+        List<WebElement> checkbox = driver.findElements(By.cssSelector("[type=checkbox]"));
+
+        Assert.assertFalse(checkbox.get(1).isSelected());
+
+        checkbox.get(1).click();
+
+        Assert.assertTrue(checkbox.get(1).isSelected());
+        Assert.assertTrue(checkbox.get(2).isSelected());
+
+        checkbox.get(2).click();
+
+        Assert.assertFalse(checkbox.get(2).isSelected());
+    }
+
     @AfterMethod(alwaysRun = true)
-    public void tearDown(){
+    public void tearDown() {
         driver.quit();
     }
 }
